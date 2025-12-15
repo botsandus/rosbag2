e2cb71058f7c871bb5192cc8a2bb11a573154b48
Tony Najjar <tony.najjar@dexory.com>
2025-12-15 15:58:20 +0000

    Revert "fix for when buffer overflows"
    This reverts commit 8feb77ce34e3501fe530e5ce52bcfb07fc0d8220.

--- rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
+++ rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
@@ -304,18 +304,6 @@ std::string SequentialWriter::format_storage_uri(
 
 void SequentialWriter::switch_to_next_storage()
 {
-  // Write transient local messages at the END of current bag before closing
-  if (pending_messages_to_republish_ && !pending_messages_to_republish_->empty()) {
-    for (const auto & msg : *pending_messages_to_republish_) {
-      if (use_cache_) {
-        message_cache_->push(msg);
-      } else {
-        storage_->write(get_writeable_message(msg));
-      }
-    }
-    pending_messages_to_republish_->clear();
-  }
-
   // consume remaining message cache
   if (use_cache_) {
     cache_consumer_->stop();
@@ -380,10 +368,15 @@ void SequentialWriter::execute_bag_split_callbacks(
   info->opened_file = opened_file;
   callback_manager_.execute_callbacks(bag_events::BagEvent::WRITE_SPLIT, info);
 
-  // Store messages to republish - they'll be written at the END of the next bag before closing
+  // Write any messages that the callback populated for republishing
   if (!info->messages_to_republish.empty()) {
-    pending_messages_to_republish_ = std::make_shared<std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>>>(
-      info->messages_to_republish);
+    for (const auto & msg : info->messages_to_republish) {
+      if (use_cache_) {
+        message_cache_->push(msg);
+      } else {
+        storage_->write(get_writeable_message(msg));
+      }
+    }
   }
 }
 
