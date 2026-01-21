0761bed49660c74127664680c6662847a1016715
Tony Najjar <tony.najjar@dexory.com>
2026-01-21 10:28:57 +0000

    fix
    Signed-off-by: Tony Najjar <tony.najjar@dexory.com>

--- rosbag2_cpp/include/rosbag2_cpp/cache/message_cache_circular_buffer.hpp
+++ rosbag2_cpp/include/rosbag2_cpp/cache/message_cache_circular_buffer.hpp
@@ -63,6 +63,14 @@ public:
   */
   bool push(CacheBufferInterface::buffer_element_t msg) override;
 
+  /**
+  * \brief Pushes a SerializedBagMessage to the front of the cache buffer.
+  * \details Prepends message without size checking - caller must ensure total size is acceptable.
+  * \param msg SerializedBagMessage to add to the front of the buffer.
+  * \return True if message was successfully pushed. Returns false if msg is null.
+  */
+  bool push_front(CacheBufferInterface::buffer_element_t msg);
+
   /// Clear buffer
   void clear() override;
 
